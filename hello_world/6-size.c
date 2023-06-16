1d13107ef7eadd7832b3cfefcab301b717dc063d
SteveCast60 <stevecast60@gmail.com>
2023-06-16 15:02:41 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %li byte(s)\n", ((unsigned long)sizeof(int));
+	printf("Size of an int: %li byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %li bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %li bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %li bytes(s)\n", sizeof(float));
