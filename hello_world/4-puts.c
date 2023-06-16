1d383570f7ca21fec6a954911f4bb9ecc68727fd
SteveCast60 <stevecast60@gmail.com>
2023-06-15 17:18:23 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-puts("\"Programming is like building a multilingual puzzle\n); return (0);
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzel");
+
+	return(0);
+}
