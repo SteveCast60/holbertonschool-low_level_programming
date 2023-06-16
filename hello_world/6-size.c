7ab90103efcb2feacbeb1c4c993ce569d0159513
SteveCast60 <stevecast60@gmail.com>
2023-06-16 10:56:22 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-#include <conio.h>
-main()
-{
-		clrscr();
-		printf("    short int is %2d bytes \n", sizeof(short int));
-		printf("          int is %2d bytes \n", sizeof(int));
-		printf("        int * is %2d bytes \n", sizeof(int *));
-		printf("     long int is %2d bytes \n", sizeof(long int));
-		printf("   long int * is %2d bytes \n", sizeof(long int *));
-		printf("   signed int is %2d bytes \n", sizeof(signed int));
-		printf(" unsigned int is %2d bytes \n", sizeof(unsigned int));
-		printf("\n");
-		printf("        float is %2d bytes \n", sizeof(float));
-		printf("      float * is %2d bytes \n", sizeof(float *));
-		printf("       double is %2d bytes \n", sizeof(double));
-		printf("     double * is %2d bytes \n", sizeof(double *));
-		printf("  long double is %2d bytes \n", sizeof(long double));
-		printf("\n");
-		printf("  signed char is %2d bytes \n", sizeof(signed char));
-		printf("         char is %2d bytes \n", sizeof(char));
-		printf("       char * is %2d bytes \n", sizeof(char *));
-		printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
-		getch();
+#include<stdio.h>
+int main() {
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+	// sizeof evaluates the size of a variable
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+                     
+    return 0;
 }
