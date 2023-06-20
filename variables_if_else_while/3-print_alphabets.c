e786a10490c194bac3fd56922c974648a7083845
SteveCast60 <stevecast60@gmail.com>
2023-06-20 07:42:21 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
- 
+
 int main()
 {
-    char i;
- 
-    printf("Capital (upper) case characters:\n");
-    for(i='A'; i<='Z'; i++)
-        printf("%c ",i);
- 
-    printf("\n\nLower case characters:\n");
-    for(i='a'; i<='z'; i++)
-        printf("%c ",i);
-     
+    for(int x = 'A'; x <= 'Z'; x++)
+    {
+        x = tolower(x);
+        putchar(x);
+    }
+    for(int x = 'a'; x <= 'z'; x++)
+    {
+	    x =toUpper(x);
+	    putchar(x);
+    }
     return 0;
 }
