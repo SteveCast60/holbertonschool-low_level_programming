8e69725e3e0c022df3d0af732a32c6c7f3ebfec8
SteveCast60 <stevecast60@gmail.com>
2023-06-20 13:50:43 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* main - entry point of program */
+
+/* description: program that prints all numbers of base 16 */
+
+int main(void)
+{
+	int digit;
+	
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar('0' + digit);
+	}
+	
+	for (digit = 0; digit < 6; digit++)
+	{
+		putchar('a' + digit);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
