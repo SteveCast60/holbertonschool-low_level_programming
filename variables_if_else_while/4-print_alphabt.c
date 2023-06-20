4315d8f6e2d8c61f4ae634bf7f8e930a71f43b4b
SteveCast60 <stevecast60@gmail.com>
2023-06-20 08:39:21 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    char letter;
+int main(void) 
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++) {
+		if (letter != 'q' && letter != 'e') 
+		{
+			putchar(letter);
+		}
+}
 
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        if (letter != 'q' && letter != 'e') {
-            putchar(letter);
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+	putchar('\n');
+	
+	return 0;
 }
