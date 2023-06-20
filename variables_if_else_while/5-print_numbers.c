53d01ab75df41c8b2738ee584eb8fd90229d8b65
SteveCast60 <stevecast60@gmail.com>
2023-06-20 08:53:07 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* main - entry point of program*/
+
+/* Description: program that prints the numbers base 10 */
+
+int main() 
+{
+	int number;
+	
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+	
+	putchar('\n');
+	
+	return 0;
+}
