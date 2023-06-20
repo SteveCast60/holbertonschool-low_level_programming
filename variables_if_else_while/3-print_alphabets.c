6b51ae5c2d76f295bde5afd3f394b97c3ab65791
SteveCast60 <stevecast60@gmail.com>
2023-06-20 08:12:40 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
-void print_alphabet() {
+int main() {
     char lowercase[27];
     char uppercase[27];
     int i;
 
+    /* Llenar el alfabeto en minúsculas */
     for (i = 0; i < 26; i++) {
         lowercase[i] = 'a' + i;
     }
     lowercase[26] = '\0';
 
+    /* Llenar el alfabeto en mayúsculas */
     for (i = 0; i < 26; i++) {
         uppercase[i] = 'A' + i;
     }
     uppercase[26] = '\0';
 
-    printf("Lowercase alphabet: %s\n", lowercase);
-    printf("Uppercase alphabet: %s\n", uppercase);
-}
+    printf("%s%s\n", lowercase, uppercase);
 
-int main() {
-    print_alphabet();
     return 0;
 }
