389e2f748db00fba25d130b574dbeb67b524a176
SteveCast60 <stevecast60@gmail.com>
2023-06-20 09:19:43 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main - Entry point of program */
+
+/* Description: program that prints all single digit numbers base 10. */
+
+int main(void)
+{
+	int x;
+
+	for ( x = 0; x < 10; x++) {
+		putchar ( x + '0');
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
