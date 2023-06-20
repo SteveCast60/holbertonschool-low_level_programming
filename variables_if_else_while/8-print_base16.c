bfaf83bab1d5662898b477e4b48044c9faf6e59a
SteveCast60 <stevecast60@gmail.com>
2023-06-20 13:53:12 -0700

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,18 +7,17 @@
 int main(void)
 {
 	int digit;
-	
+
 	for (digit = 0; digit < 10; digit++)
 	{
 		putchar('0' + digit);
 	}
-	
+
 	for (digit = 0; digit < 6; digit++)
 	{
 		putchar('a' + digit);
 	}
-	
+
 	putchar('\n');
-	
 	return (0);
 }
