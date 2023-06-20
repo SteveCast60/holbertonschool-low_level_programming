53e3167bb119cdc83546f697416710a1b42a8151
SteveCast60 <stevecast60@gmail.com>
2023-06-20 08:58:06 -0700

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -4,16 +4,16 @@
 
 /* Description: program that prints the numbers base 10 */
 
-int main() 
+int main(void)
 {
 	int number;
-	
+
 	for (number = 0; number < 10; number++)
 	{
 		putchar(number + '0');
 	}
-	
+
 	putchar('\n');
-	
+
 	return 0;
 }
