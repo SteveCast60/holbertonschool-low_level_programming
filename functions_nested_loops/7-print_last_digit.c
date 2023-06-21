4d40ff54ef241c6308df54f140da8ea30e97d27f
SteveCast60 <stevecast60@gmail.com>
2023-06-21 13:57:48 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point of progrram
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
+
