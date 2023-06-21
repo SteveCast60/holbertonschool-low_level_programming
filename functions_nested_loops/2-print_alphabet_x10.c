1ad8e7f2712d6770ddc038506971a66aa4774f6d
SteveCast60 <stevecast60@gmail.com>
2023-06-21 09:59:46 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ * main - entry point of program
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+
+	putchar('\n');
+}
