063bf2a4accf1159fd17ed6830efaad8d09f7f24
SteveCast60 <stevecast60@gmail.com>
2023-06-21 12:16:47 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  _isalpha - entry point of program
+ *
+ *  return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
