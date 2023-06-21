b0d63416c45598a3bfefef6567f8a3cb4b6beb10
SteveCast60 <stevecast60@gmail.com>
2023-06-21 08:52:33 -0700

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* main - entry point of program */
 
-/* Description: program that prints the lowercase alphabet in reverse */
+/**
+ * main - entry point of program
+ * Return: 0
+ */
 
 int main(void)
 {
