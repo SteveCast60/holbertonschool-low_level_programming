d64383884e23cf4f7db31a8b6225078e92bfa4ba
SteveCast60 <stevecast60@gmail.com>
2023-06-24 09:29:06 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,27 +2,12 @@
 #include "main.h"
 
 /**
- * _putchar - function
- * @c: parameter of function
  * print_most_numbers - Entry point of program
  * Return: 0
  */
 
-void _putchar(char c)
-{
-
-}
-
 void print_most_numbers()
 {
-	int i;
-	
-	for (i = 0; i <= 9; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar('0' + i);
-		}
-	}
-	_putchar('\n');
+	char numbers[] = "01356789\n";
+	printf("%s", numbers);
 }
