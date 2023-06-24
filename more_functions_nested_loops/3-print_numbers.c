32a9fc3a50d8f7566fb41ceb74ee1da620d8aa19
SteveCast60 <stevecast60@gmail.com>
2023-06-24 08:59:06 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ *print_numbers - Entry point of program
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+}
