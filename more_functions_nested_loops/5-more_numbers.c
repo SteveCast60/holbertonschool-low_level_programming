e224493fa4147681fc7a3ec8a5a263c2182c4c69
SteveCast60 <stevecast60@gmail.com>
2023-06-24 09:44:40 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@
 void more_numbers()
 {
 	char numbers[] = "01234567891011121314\n";
-	int i, j;
+	int j;
 
 	for (j = 0; j < 10; j++)
 	{
-		printf("%c", char[j]);
+		printf("%c", numbers[j]);
 	}
 }
