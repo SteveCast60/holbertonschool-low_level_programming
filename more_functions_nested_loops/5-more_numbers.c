c2d81f136921ee70a0e20f9d49ef09993c07c167
SteveCast60 <stevecast60@gmail.com>
2023-06-24 09:54:37 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,10 +17,10 @@ void more_numbers(void)
 		{
 			if (y > 9)
 			{
-				_putchar((y / 10) + '0');
+				putchar((y / 10) + '0');
 			}
-			_putchar((y % 10) + '0');
+			putchar((y % 10) + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
