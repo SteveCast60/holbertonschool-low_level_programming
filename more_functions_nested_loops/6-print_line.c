b6c6f66234147a7456d57e38d8c9ceb376294043
SteveCast60 <stevecast60@gmail.com>
2023-06-24 10:13:11 -0700

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,21 +6,20 @@
  * Return: 0
  */
 
-int print_line(int n)
+void print_line(int n)
 {
 	int x;
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
 		for (x = 0; x < n; x++)
 		{
-			_putchar(95);
+			putchar(95);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
-
