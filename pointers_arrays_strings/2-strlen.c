e603adfe1468c9fae250a4f226c41cb398431514
SteveCast60 <stevecast60@gmail.com>
2023-06-26 15:22:46 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - function count length of a string
+ * @s: pointer of function
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
+}
