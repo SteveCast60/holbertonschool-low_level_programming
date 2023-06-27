c6fca819a40f29799f8d6be19e9cee27ec5b24d5
SteveCast60 <stevecast60@gmail.com>
2023-06-27 13:09:55 -0700

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - entry point of program
+ *@s: argument of function
+ *Return: 0
+ */
+
+void rev_string(char *s)
+{
+	if (s == NULL)
+		return;
+	
+	int length = strlen(s);
+	int start = 0;
+	int end = length - 1;
+	
+	while (start < end) 
+	{
+		char temp = s[start];s
+		[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
