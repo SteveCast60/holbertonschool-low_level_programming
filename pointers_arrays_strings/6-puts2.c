0eba68bd861c95f2dd38e57918d9d6329bcc2bf3
SteveCast60 <stevecast60@gmail.com>
2023-06-27 14:38:25 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,19 @@
 
 /**
  * puts2 - function prints character of a string
- * @str: argument of function 
+ * @str: argument of function
  * Return: 0
  */
 
 void puts2(char *str)
 {
 	int i;
+	
 	i = 0;
 
 	if (str == NULL)
 	return;
-	
+
 	while (str[i] != '\0')
 	{
 		printf("%c", str[i]);
