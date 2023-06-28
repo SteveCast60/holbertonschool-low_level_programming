b873724e22aef5be7e3dcfce8ac2481342f56c1a
SteveCast60 <stevecast60@gmail.com>
2023-06-28 14:32:21 -0700

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	strcpy(src, dest);
+
+	puts(str2);
+
+	return (dest);
+}
