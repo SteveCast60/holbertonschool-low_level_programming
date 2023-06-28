a47cd7455b1fea45333f5fc7934768b3ed6e8893
SteveCast60 <stevecast60@gmail.com>
2023-06-28 12:50:24 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,9 +14,10 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++);
 
+
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i]=src[j];
+		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
