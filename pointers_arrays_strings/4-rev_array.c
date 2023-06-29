d8ef587cb224e461699fae356b57f4b87a00a6a3
SteveCast60 <stevecast60@gmail.com>
2023-06-29 12:26:15 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 
 /**
- *
- *
+ *reverse_array - function that reverse a array
+ *@a: array that pass a string
+ *@n: iterancy number of array
+ *Return: nothing
  */
 void reverse_array(int *a, int n)
 {
 	int i, temp;
-	
-	for (i = 0; i < n/2; i++)
+
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n-1-i];
-		a[n-1-i] = temp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
