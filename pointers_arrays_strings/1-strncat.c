ef558f35383c96cb24d4acb1d074d94fa9a79c40
SteveCast60 <stevecast60@gmail.com>
2023-06-29 13:29:44 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * Author: SteveCast
- * Description - Program that Concatenates two strings
- * _strncat - function concatenates two strings
- * @dest: parameter of function
- * @src: argm function
- * @n: argm function
- * Return: Pointer *dest
+/** 
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
