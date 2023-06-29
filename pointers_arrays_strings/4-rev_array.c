c9f0fa9d49c10b5bfef675e2a9275461990b4984
SteveCast60 <stevecast60@gmail.com>
2023-06-29 12:13:52 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+void reverse_array(int *a, int n);
+{
+	int temp;
+	int start;
+	start = 0;
+
+	if (start >= n)
+		return;
+
+	temp = a[start];
+	a[start] = a[n];
+	a[n] = temp;
+}
