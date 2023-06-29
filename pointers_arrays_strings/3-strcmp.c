6a1113a707609fa19d37acb1b4b8462ca0fb1ea5
SteveCast60 <stevecast60@gmail.com>
2023-06-29 11:37:30 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strcmp - function compares two strings
+ * @s1: parameter of function
+ * @s2: argument of function
+ * Return: 0
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
