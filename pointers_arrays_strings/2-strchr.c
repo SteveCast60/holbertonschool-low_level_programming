c33cd6e67ae36b8ceda835c300cf3dc480d8e4c2
SteveCast60 <stevecast60@gmail.com>
2023-06-30 13:11:23 -0700

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != NULL)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		while (*s == '\0')
-		{
-			return (s);
-		}
+	
 	}
 	return (NULL);
 }
