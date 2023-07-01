b27d87affa4556b88c4b8611c85cc3ef558772cd
SteveCast60 <stevecast60@gmail.com>
2023-07-01 16:06:14 -0700

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,22 @@
  * Return: always 0 (success)
  */
 
-unsigned int _strspn(char* s, char* accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
-	value = 0;
-
-	for (i = 0; s[i] !='\0'; i++)
-	{
-		check = 0;
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
-		}
-	}
-}	
+    int count = 0;
+ 
+    // Iterating string T.
+    for (int i = 0; i < strlen(accept); i++)
+    {
+        // If end of string S.
+        if (count == strlen(s))
+            break;
+ 
+        // If character match,
+        // increment counter.
+        if (accept[i] == s[count])
+            count++;
+    }
+ 
+    return count;
+}
