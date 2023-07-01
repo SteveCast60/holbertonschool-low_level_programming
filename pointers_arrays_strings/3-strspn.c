556b6a8be554a9fb5bd3ab57b5428dee93712f9b
SteveCast60 <stevecast60@gmail.com>
2023-07-01 16:46:20 -0700

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	char
-	int i;
+	char *s = accept;
 	int cont;
 	int result;
 
-	for (i = 0; i < accept && accept != '\0'; i++)
+	while (*s != '\0')
 	{
-		while (*s == *accept[i])
-		{
-			result = (cont + 1);
-		}
+		result = (cont + 1);
 	}
 	return (result);
 }
