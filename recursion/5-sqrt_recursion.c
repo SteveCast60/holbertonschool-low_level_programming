8ec4d33d0b2c8b52f71a06055fbbe2bade6b69a2
SteveCast60 <stevecast60@gmail.com>
2023-07-03 14:04:05 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 
-
+/**
+ * _sqrt_recursion_helper - entry point
+ * @n: input
+ * @start: input
+ * @end: input
+ * Return: Always 0 (success)
+ */
 
 int _sqrt_recursion_helper(int n, int start, int end)
 {
-	if (start <= end)
+	if (start > end)
 	{
-		int mid = (start + end) / 2;
-		int square = mid * mid;
-		if (square == n)
-		{
-			return (mid);
-		}
-		else if (square < n)
-		{
-			return (_sqrt_recursion_helper(n, mid + 1, end));
-		}
-		else
-		{
-			return (_sqrt_recursion_helper(n, start, mid - 1));
-		}
+		return (-1);
 	}
-	return (-1);
+	int mid = (start + end) / 2;
+	int square = mid * mid;
+
+	if (square == n)
+	{
+		return (mid);
+	}
+	else if (square < n)
+	{
+		return (_sqrt_recursion_helper(n, mid + 1, end));
+	}
+	else 
+	{
+		return (_sqrt_recursion_helper(n, start, mid - 1));
+	}
 }
+
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
 	}
 	return (_sqrt_recursion_helper(n, 0, n));
 }
