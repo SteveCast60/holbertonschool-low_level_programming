7413df39a36fb798229a52ae26b11522f7b85608
SteveCast60 <stevecast60@gmail.com>
2023-07-03 12:49:11 -0700

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - entry point
+ * @s: input value
+ * Return: Always 0 (success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
