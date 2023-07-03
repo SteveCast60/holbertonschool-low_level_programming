9fd4052554df4798b5dd872e56136fad4850a441
SteveCast60 <stevecast60@gmail.com>
2023-07-03 14:34:10 -0700

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * function that checks if an input integer n is a prime 
- * _is_prime_number: function that checks if an input integer n is a prime 
+ * function that checks if an input integer n is a prime
+ * _is_prime_number: function that checks if an input integer n is a prime
  * _is_prime_number_helper: Entry point of program
  * @n: input
  * @divisor: input
