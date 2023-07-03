00e22ecec2395d88af50a5dc4ba58e0a318001e9
SteveCast60 <stevecast60@gmail.com>
2023-07-03 14:26:37 -0700

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * _is_prime_number - entry point of program
- * @n: input
- * _is_prime_number_helper - auxiliar function
+ * _is_prime_number: entry point of program
+ * _is_prime_number_helper: auxiliar function
  * @n: input
  * @divisor: input
  * Return: Always 0 (success)
@@ -21,11 +20,11 @@ int is_prime_number_helper(int n, int divisor)
 	}
 	if (n % divisor == 0)
 	{
-		return 0;
+		return (0);
 	}
-	return is_prime_number_helper(n, divisor + 1);
+	return (is_prime_number_helper(n, divisor + 1));
 }
 int is_prime_number(int n)
 {
-	return is_prime_number_helper(n, 2);
+	return (is_prime_number_helper(n, 2));
 }
