88feec902130edb12f4a5c88598569fc15f8c88a
SteveCast60 <stevecast60@gmail.com>
2023-07-06 14:16:16 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - entry point of program
- * @n: input
  * _sqrt_recursion_aux - function that returns the natural squared root
  * @n: input
  * @y: input
@@ -25,6 +23,12 @@ int _sqrt_recursion_aux(int n, int y)
 	}
 	return (_sqrt_recursion_aux((n), y++));
 }
+
+/**
+ * _sqrt_recursion - entry point of program
+ * @n: input
+ * Return: 0
+ */
 
 int _sqrt_recursion(int n)
 {
