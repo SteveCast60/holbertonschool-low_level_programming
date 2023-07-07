847a51967f2b20e6a3a0109e879fda9e525f6f32
SteveCast60 <stevecast60@gmail.com>
2023-07-07 15:05:36 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,12 +18,12 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-	
+
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	r = x * y;
 	printf("%d\n", r);
-	return(0);
+	return (0);
 }
