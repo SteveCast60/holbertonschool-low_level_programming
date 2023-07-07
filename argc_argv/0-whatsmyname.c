8e3736001c85c9e2d8c05fcac436139739bdbaff
SteveCast60 <stevecast60@gmail.com>
2023-07-07 09:55:17 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+
+/**
+ * main - print name of program
+ * @argc: index of arguments
+ * @argv: vector of argc
+ * Return: Always 0 (success)
+ */
+
+int main (int argc, char *argv[])
+{
+	        printf("%s\n", argv[0]);
+		        return (0);
+}
