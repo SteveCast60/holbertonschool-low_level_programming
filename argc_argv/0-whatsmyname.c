969177dd6c9783ef7b62c3e225f8eb3dcc14e534
SteveCast60 <stevecast60@gmail.com>
2023-07-07 10:05:07 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,11 +7,11 @@
  * Return: Always 0 (success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc >= 0)
 	{
-	        printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
