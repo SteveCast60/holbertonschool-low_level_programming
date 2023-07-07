c5fe962a3211ca06a2816beee4176260c11e953f
SteveCast60 <stevecast60@gmail.com>
2023-07-07 10:18:26 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: index of arguments
+ * @argv: vector of argc
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	if (argc >= 0)
+	{
+		for (i = 0; i < *argv[0]; i++);
+		{
+			*argv++
+		}
+		printf("%s\n", i);
+	}
+	return (0);
+}
+
+
