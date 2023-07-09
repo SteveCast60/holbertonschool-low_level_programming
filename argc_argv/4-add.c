7d957595afdbc9c98a5367908be13f1da9b4f973
SteveCast60 <stevecast60@gmail.com>
2023-07-09 10:16:12 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This funtion adds positive numbers
+ * @argc : argument
+ * @argv : array of pointer to string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, suma = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		suma += atoi(argv[i]);
+	}
+	printf("%d\n", suma);
+	return (0);
+}
