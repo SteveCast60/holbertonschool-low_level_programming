464598434ed323e5e7d3c686493ef17643b94c88
SteveCast60 <stevecast60@gmail.com>
2023-07-10 15:28:25 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	size_t length = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc((length + 1) * sizeof(char));
+	strcpy(ptr, str);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+return (ptr);
+}
