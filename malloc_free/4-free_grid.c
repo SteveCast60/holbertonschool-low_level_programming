ec3ea302fbd593049ff8efb6610a4b304d6d4abb
SteveCast60 <stevecast60@gmail.com>
2023-07-11 14:35:44 -0700

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_grid - functions that free the memory
+ *@grid: pointer a pointer
+ *@height: input array size
+ *Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+  free(grid[i]);
+}
+free(grid);
+}
