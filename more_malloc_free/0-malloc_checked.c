434ceb23fdd20beb00d616413b9ac74de6b663dd
SteveCast60 <stevecast60@gmail.com>
2023-07-11 15:51:30 -0700

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,16 @@
 /**
  *malloc_checked - function that allocates memory
  *@b: input value
- *return: pointer
+ *Return: 0
  */
 
 void *malloc_checked(unsigned int b)
 {
-        void *ptr = malloc(b);
-		
+	void *ptr = malloc(b);
+
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-        return (ptr);
+	return (ptr);
 }
