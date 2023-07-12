45f7d1e6141ca3db300e1bec3132b180b2b752cf
SteveCast60 <stevecast60@gmail.com>
2023-07-12 14:04:00 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		((char * ) ptr) [i] = 0;
+	}
+	return (ptr);
+}
