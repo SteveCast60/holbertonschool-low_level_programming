2d1830a2ad2556cac9f6d8b92bcf854c32d1d1e0
SteveCast60 <stevecast60@gmail.com>
2023-07-12 14:17:56 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *_calloc - function that allocates memory for an array
+ *@nmemb: input value
+ *@size: input value
+ *Return: ptr pointer
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
@@ -14,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		((char * ) ptr) [i] = 0;
+		((char *)ptr)[i] = 0;
 	}
 	return (ptr);
 }
