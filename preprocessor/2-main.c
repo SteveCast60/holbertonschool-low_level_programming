4f42595f385846eb5a1e760f30b60c955e29c28d
SteveCast60 <stevecast60@gmail.com>
2023-07-13 16:46:06 -0700

    2-main.c

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Compiled from file: %s\n", __FILE__);
-    return 0;
+/**
+ *main - program that print the name of the file
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
 }
