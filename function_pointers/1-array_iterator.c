e431d6321670c53a8f86ae17d953853dc0ee3540
SteveCast60 <stevecast60@gmail.com>
2023-07-18 16:30:54 -0700

    0-print_name.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	i = 0;
+
+	if (action != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
