f0a295696d8dc62dd61ec4c3f5fc8e4e1f5c418d
SteveCast60 <stevecast60@gmail.com>
2023-07-19 09:10:37 -0700

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -23,9 +23,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	for (i = 0; i < size; i++)
 	{
-		(cmp(array[c]) != 0);
+		(cmp(array[i]) != 0);
 		{
-			return (c);
+			return (i);
 		}
 	}
 	return (-1);
