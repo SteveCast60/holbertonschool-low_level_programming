2f8c8ccb50ef2860909006e7d89251a32de661aa
SteveCast60 <stevecast60@gmail.com>
2023-07-21 10:41:44 -0700

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,19 +6,20 @@
  *print_numbers - function that prints all numbers
  *@n: input value
  *@separator: value strings
- *Return: void 
+ *Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i, x;
 	va_list num;
+
 	va_start(num, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		x = va_arg(num, unsigned int);
-		printf("%d", x);
+		printf ("%d", x);
 		if(i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
