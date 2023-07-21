0efda154cfb507dc1c96b0ecea6dcec2e2b0903b
SteveCast60 <stevecast60@gmail.com>
2023-07-20 18:09:57 -0700

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,10 +3,9 @@
 #include <stdarg.h>
 
 /**
- *
- *
- *
- *
+ *sum_them_all - function that sum all arguments
+ *@n: input value
+ *Return: sum of all arguments
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -14,15 +13,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum_nums_all = 0;
 	va_list args;
-        va_start(args, n);
+	va_start(args, n);
 
 	if (n == 0)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		int num = va_arg(args, int);
+
 		sum_nums_all += num;
 	}
 	va_end(args);
