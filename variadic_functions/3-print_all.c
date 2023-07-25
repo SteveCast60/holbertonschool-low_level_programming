2f1819c19666513b974f1d68c2ec420a3796b0c8
SteveCast60 <stevecast60@gmail.com>
2023-07-25 10:41:20 -0700

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,94 +1,121 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
+
+
 
 /**
- *variables: struct
- */
-typedef struct variables
-        {
-        char *ptr;
-        void (*function)(va_list argts);
-        } type_var;
+ *
+ *  * p_char - print char
+ *
+ *   * @argts: list of arguments
+ *
+ *    */
 
-/**
- * p_char - print char
- * @argts: list of arguments
- */
 void p_char(va_list argts)
+
 {
-        printf("%c", va_arg(argts, int));
+
+	printf("%c", va_arg(argts, int));
+
 }
+
 /**
- * p_int - print int
- * @argts: list of arguments
- */
+ *
+ *  * p_int - print int
+ *
+ *   * @argts: list of arguments
+ *
+ *    */
+
 void p_int(va_list argts)
+
 {
-        printf("%d", va_arg(argts, int));
+
+	printf("%d", va_arg(argts, int));
+
 }
+
 /**
- * p_float - print float
- * @argts: list of arguments
- */
+ *
+ *  * p_float - print float
+ *
+ *   * @argts: list of arguments
+ *
+ *    */
+
 void p_float(va_list argts)
+
 {
-        printf("%f", va_arg(argts, double));
+
+	printf("%f", va_arg(argts, double));
+
 }
+
 /**
- * p_str - print string
- * @argts: list of arguments
- */
+ *
+ *  * p_str - print string
+ *
+ *   * @argts: list of arguments
+ *
+ *    */
+
 void p_str(va_list argts)
-{
-        char *str = va_arg(argts, char *);
 
-        if (str == NULL)
-        {
-        printf("(nil)");
-        return;
-        }
-        printf("%s", str);
+{
+
+	char *str = va_arg(argts, char *);
+
+
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 }
 
 /**
- * print_all - entry point
- * @format: input value
- * Return: void
- */
+ *
+ *  * print_all - prints anything
+ *
+ *   * @format: list of types of arguments
+ *
+ *    */
+
+
+
 void print_all(const char * const format, ...)
 {
 	int i = 0, x;
 	char *esp = "", *coma = ", ";
 	va_list argts;
+	
+	type_var variables[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+	
+	va_start(argts, format);
+	
+	while (format && format[i])
+	{
+		x = 0;
+		while (x < 4)
+	{
+	if (format[i] == *(variables[x].ptr))
+	{
+		printf("%s", esp);
+		variables[x].function(argts);
+		esp = coma;
+		x++;
+	}
+	}		
 
-        type_var variables[] = {
-                {"c", p_char},
-                {"i", p_int},
-                {"f", p_float},
-                {"s", p_str},
-                {NULL, NULL}
-        };
-
-        va_start(argts, format);
-
-        while (format[i])
-        {
-        x = 0;
-                while (x < 4 && format[i] != *(variables[x].ptr))
-
-                  x++;
-                        if (x < 4)
-                        {
-                                printf("%s", esp);
-                                variables[x].function(argts);
-                                esp = coma;
-                        }
-                        i++;
-                
-                
-        }
-        printf("\n");
-        va_end(argts);
+	i++;
+	}
+	printf("\n");
+	va_end(argts);
 }
-
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -7,6 +12,26 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ *
+ *  * struct variables - Struct variables
+ *
+ *   *
+ *
+ *    * @ptr: The format
+ *
+ *     * @function: The function associated
+ *
+ *      */
 
+
+
+typedef struct variables
+
+{
+	char *ptr;
+	void (*function)(va_list argts);
+
+} type_var;
 
 #endif
