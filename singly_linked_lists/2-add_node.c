296836f16ac221988c0843bc9e65e9c05cf99f29
SteveCast60 <stevecast60@gmail.com>
2023-08-03 14:05:25 -0700

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,26 +9,21 @@ typedef struct node {
 
 list_t *add_node(list_t **head, const char *str) {
     if (str == NULL) {
-        return NULL; // Invalid input
+        return NULL;
     }
 
-    // Allocate memory for the new node
-    list_t *new_node = (list_t *)malloc(sizeof(list_t));
+    *new_node = (list_t *)malloc(sizeof(list_t));
     if (new_node == NULL) {
-        return NULL; // Memory allocation failure
+        return NULL; 
     }
 
-    // Duplicate the input string
     new_node->str = strdup(str);
     if (new_node->str == NULL) {
         free(new_node);
-        return NULL; // Memory allocation failure
+        return NULL;
     }
 
-    // Update the next pointer of the new node to point to the current head
     new_node->next = *head;
-
-    // Update the head to point to the new node
     *head = new_node;
 
     return new_node;
