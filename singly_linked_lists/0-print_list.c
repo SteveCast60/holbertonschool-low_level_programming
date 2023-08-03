b13a2c96ca461e5c713b55b078beb1bbbef902b3
SteveCast60 <stevecast60@gmail.com>
2023-08-03 11:03:47 -0700

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,33 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <stddef.h>
+//#include "lists.h"
 
 typedef struct list_s
 {
-	char *str;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	printf("[");
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-		{
-			printf("0");
-		}
-		else
-		{
-			printf("%s", h->str);
-		}
-		h = h->next;
-		if (h != NULL)
-		{
-			printf(", ");
-		}
-		count++;
-	}
-	printf("]\n");
-	return count;
+  size_t i = 0;
+  
+  const list_t *current = h;
+  
+  while (current != NULL)
+  {
+    if(current->str == NULL)
+    {
+      printf("[0] (nil)\n");
+    }
+    else
+    {
+      printf("[%u] %s\n", current->len, current->str);
+    }
+    current = current->next;
+    i++;
+  }
+  
+  return (h);
 }
