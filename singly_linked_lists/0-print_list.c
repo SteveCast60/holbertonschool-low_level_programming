72b7ccd0b081d5249a767b48eed5c83ba937a06c
SteveCast60 <stevecast60@gmail.com>
2023-08-03 11:29:16 -0700

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,29 +4,28 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
+ * print_list - check the code
+ *@h: input value
  * Return: Always 0.
  */
 
 size_t print_list(const list_t *h)
 {
-  size_t i = 0;
-  const list_t *current = h;
-
-  while (current != NULL)
-  {
-    if (current->str == NULL)
-    {
-      printf("[0] (nil)\n");
-    }
-    else
-    {
-      printf("[%u] %s\n", current->len, current->str);
-    }
-    current = current->next;
-    i++;
-  }
-
-  return (i);
+	size_t i = 0;
+	const list_t *current = h;
+	
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		i++;
+	}
+	return (i);
 }
