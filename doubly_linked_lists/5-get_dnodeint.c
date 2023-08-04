9b53b806d232981f727267078ab13ba8b5f6718a
SteveCast60 <stevecast60@gmail.com>
2023-08-04 09:29:08 -0700

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,26 +4,30 @@
 #include "lists.h"
 
 /**
- *  * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
- *   * @head: Pointer to the head of the doubly linked list.
- *    * @index: Index of the node to retrieve (starting from 0).
- *     *
- *      * Return: Pointer to the nth node, or NULL if the node does not exist.
- *       */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+ *get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+ *@head: Pointer to the head of the doubly linked list.
+ *@index: Index of the node to retrieve (starting from 0).
+ *
+ *Return: Pointer to the nth node, or NULL if the node does not exist.
+ */
+
+typedef struct dlistint_s
 {
-	    dlistint_t *current = head;
-	        unsigned int count = 0;
+	    int n;
+	        struct dlistint_s *prev;
+		    struct dlistint_s *next;
+} dlistint_t;
 
-		    while (current != NULL)
-			        {
-					        if (count == index)
-							            return current;
-
-						        current = current->next;
-							        count++;
-								    }
-
-		        return NULL; // Index out of range or empty list
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int i = 0;
+	const dlistint_t *current = head;
+	while (current != NULL && i < index)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+	return (current);
 }
-
