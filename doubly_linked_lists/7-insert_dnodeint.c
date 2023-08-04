b32f485753df49e1488e8b6ced861251a20cdf8c
SteveCast60 <stevecast60@gmail.com>
2023-08-04 11:26:21 -0700

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,35 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	while (h != NULL)
+	if (h == NULL || idx == 0)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		return add_dnodeint(h, n);
 	}
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return NULL;
+	}
+	new_node->n = n;
+	dlistint_t *current = *h;
+	while (idx > 1 && current != NULL)
+	{
+		current = current->next;
+		idx--;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+	{
+		current->next->prev = new_node;
+	}
+	current->next = new_node;
+	return new_node;
 }
