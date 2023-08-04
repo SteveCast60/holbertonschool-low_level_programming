5f281a8058a1523f30f5cb21aa825c2fdd6f4b6d
SteveCast60 <stevecast60@gmail.com>
2023-08-04 09:55:42 -0700

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -16,14 +16,15 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
+
 	while (head != NULL)
 	{
 		if (count == index)
 		{
-			return head;
+			return (head);
 		}
 		head = head->next;
 		count++;
 	}
-	return NULL;
+	return (NULL);
 }
