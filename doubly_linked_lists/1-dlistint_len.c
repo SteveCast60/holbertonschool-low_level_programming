3fdc6c010cdf09a0e1de1fa6b79612e63d4cd6bc
SteveCast60 <stevecast60@gmail.com>
2023-08-03 18:49:44 -0700

    1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,12 +9,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t node_count = 0;
-    while (h != NULL)
-    {
-        node_count++;
-        h = h->next;
-    }
-    return node_count;
-}
+	size_t node_count = 0;
 
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+	return node_count;
+}
