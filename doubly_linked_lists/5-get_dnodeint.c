529e9b565c98500fadd9317bae800c01f6cb929c
SteveCast60 <stevecast60@gmail.com>
2023-08-04 09:42:49 -0700

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,15 +11,27 @@
  *Return: Pointer to the nth node, or NULL if the node does not exist.
  */
 
+size_t print_dlistint(const dlistint_t *h)
+{
+	    size_t node_count = 0;
+	        while (h != NULL)
+			    {
+				            printf("%d\n", h->n);
+					            h = h->next;
+						            node_count++;
+							        }
+		    return node_count;
+}
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	const dlistint_t *current = head;
-	while (current != NULL && i < index)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-		i++;
-	}
-	return (current);
+	    unsigned int i = 0;
+	        const dlistint_t *current = head;
 
+		    while (current != NULL && i < index)
+			        {
+					        current = current->next;
+						        i++;
+							    }
+		        return (dlistint_t *)current;
+}
