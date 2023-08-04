9a88ab73cf8472ae5b4d907c864fe7f26b120355
SteveCast60 <stevecast60@gmail.com>
2023-08-03 17:49:34 -0700

    3-add_node_end.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,7 +21,6 @@ typedef struct list_s
 } list_t;
 
 unsigned int _strlen(char *s);
-void print_list(const list_t *h);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
