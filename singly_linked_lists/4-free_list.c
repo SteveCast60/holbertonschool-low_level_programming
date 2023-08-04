568adb2448ba688e3b285b4a2681b81324d1a57d
SteveCast60 <stevecast60@gmail.com>
2023-08-03 17:58:50 -0700

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,15 +7,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current, *next_node;
+	list_t *current, *next_node;
 
-    current = head;
-    while (current != NULL)
-    {
-        next_node = current->next;
-        free(current->str);
-        free(current);
-        current = next_node;
-    }
+	current = head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
 }
-
