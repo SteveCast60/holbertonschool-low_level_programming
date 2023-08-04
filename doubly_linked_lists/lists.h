b74fac832a204dcac3141f9c4db758c140f3201c
SteveCast60 <stevecast60@gmail.com>
2023-08-04 11:15:56 -0700

    7-insert_dnodeint.c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -13,6 +13,7 @@ typedef struct dlistint_s
 
 /* Prototipo de la función para imprimir todos los elementos de la lista */
 void print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
