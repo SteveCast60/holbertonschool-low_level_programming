8aeb545055b3f9d2fb02268f7b25f1937c7b0d00
SteveCast60 <stevecast60@gmail.com>
2023-08-05 10:32:07 -0700

    3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-// Función para establecer un bit específico en un número dado
-void set_bit(unsigned long int *num, int bit_position) {
-    unsigned long int mask = 1UL << bit_position;
-    *num |= mask;
+/**
+ * set_bit - entry point
+ * @num: pointer
+ * @bit_position: input
+ * Return: Always 0 (success)
+ */
+
+void set_bit(unsigned long int *num, int bit_position)
+{
+	unsigned long int mask = 1UL << bit_position;
+	*num |= mask;
 }
