73967d4e9a385be0aabfd6919b2e131b75e2e2df
SteveCast60 <stevecast60@gmail.com>
2023-08-05 09:18:47 -0700

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,20 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: is the integer
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	int i;
-	int bits = sizeof(unsigned long int) * 8;
-
-	for (i = bits - 1; i >= 0; i--)
-	{
-		unsigned long int mask = 1UL << i;
-		if (n & mask)
-		{
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
-	}
+        if (n > 1)
+                print_binary(n >> 1);
+        _putchar((n & 1) + '0');
 }
