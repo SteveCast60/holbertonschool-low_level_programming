e11297a99f3729c395e5a55f5c1e8c5ee2a4b73d
SteveCast60 <stevecast60@gmail.com>
2023-08-05 09:15:20 -0700

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-void print_binary(unsigned long int n) {
-    int bits = sizeof(unsigned long int) * 8; // Number of bits in unsigned long int
+void print_binary(unsigned long int n)
+{
+	int i;
+	int bits = sizeof(unsigned long int) * 8;
 
-    // Start from the most significant bit and shift right to the least significant bit
-    for (int i = bits - 1; i >= 0; i--) {
-        unsigned long int mask = 1UL << i; // Create a mask with the bit at position i set to 1
-
-        // Check if the bit at position i is set in the number n
-        if (n & mask) {
-            printf("1");
-        } else {
-            printf("0");
-        }
-    }
+	for (i = bits - 1; i >= 0; i--)
+	{
+		unsigned long int mask = 1UL << i;
+		if (n & mask)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+	}
 }
