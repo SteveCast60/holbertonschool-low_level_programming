ea674a557d0d5cf0b1e5f0521c6954c2a0ff1194
SteveCast60 <stevecast60@gmail.com>
2023-08-05 10:24:35 -0700

    3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int set_bit(unsigned long int *n, unsigned int index) {
-    unsigned long int mask;
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
 
-    if (index >= sizeof(unsigned long int) * 8) {
-        return (-1);
-    }
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
 
-    unsigned long int mask = 1UL << index;
-
-    *n |= mask;
-
-    return (1);
+	unsigned long int mask = 1UL << index;
+	*n |= mask;
+	return (1);
 }
