22b4eb12e31614321e9777d86a6dfb4f42106971
SteveCast60 <stevecast60@gmail.com>
2023-08-05 09:36:23 -0700

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * get_bit - entry point
+ * @n: input
+ * @index: input
+ * Return: Always 0 (success)
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-        if (index >= (sizeof(unsigned long int) * 8))
-                return (-1);
-        if ((n & (1 << index)) == 0)
-                return (0);
-        return (1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
 }
